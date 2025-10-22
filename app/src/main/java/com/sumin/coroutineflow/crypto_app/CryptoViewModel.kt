15535ec18730e540aca96326dfec4d5e9187c477
Andrewka <Uzzbagoyzza@yandex.ru>
2025-10-22 20:48:43 +0300

    приминил map и asLiveDate.

--- app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/sumin/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -3,37 +3,25 @@ package com.sumin.coroutineflow.crypto_app
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
+    val state: LiveData<State> = repository.getCurrencyList()
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
+        .filter { currencyList ->
+            currencyList.isNotEmpty()
+        }
+        .map { State.Content(currencyList = it) as State }
+        .onStart {
+            State.Loading
+        }
+        .asLiveData()
 
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-            repository.getCurrencyList()
-                .onStart {
-                    val currentState = _state.value
-                    if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                        _state.value = State.Loading
-                    }
-                }
-                .filter { currencyList ->
-                    currencyList.isNotEmpty()
-                }
-                .onEach { currencyList ->
-                    _state.value = State.Content(currencyList = currencyList)
-                }
-                .launchIn(viewModelScope)
-    }
 }
